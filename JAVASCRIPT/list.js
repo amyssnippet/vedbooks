e4535bd114c436db6c559e93da5fef870075a846
Amol Yadav <amyssnipet@yahoo.com>
2024-05-29 15:34:20 +0530

    Create list.js

---
+++ JAVASCRIPT/list.js
@@ -0,0 +1,18 @@
+
+
+const articles = [
+  {
+    title: "Bhagavad Gita",
+    url: "https://www.example.com/article1",
+    image: "/Bhagavad Gita_20240511_142124_0000.png",
+    description: "A 700-verse Hindu scripture in Sanskrit."
+  },
+  {
+    title: "Mahabharata",
+    url: "https://www.example.com/article2",
+    image: "/91e52deb-51a0-4759-81b4-9211d83d7c50.jpeg",
+    description: "An ancient Indian epic where the main story revolves around the Pandavas and Kauravas."
+  },
+];
+
+export default articles;
