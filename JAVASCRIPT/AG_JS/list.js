67562ee04c651c3effb425a4e6dbc48de4248250
Amol Yadav <amyssnipet@yahoo.com>
2024-06-01 10:44:15 +0530

    Create list.js

---
+++ JAVASCRIPT/AG_JS/list.js
@@ -0,0 +1,430 @@
+// list.js
+
+const pages = [
+  {
+    imageUrl: "https://ashtavakra.vercel.app/0.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/1.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/2.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/3.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/4.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/5.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/6.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/7.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/8.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/9.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/10.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/11.jpg",
+  },
+  {
+    imageUrl: "https://ashtavakra.vercel.app/12.jpg",
+  },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/13.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/14.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/15.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/16.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/17.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/18.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/19.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/20.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/21.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/22.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/23.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/24.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/25.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/26.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/27.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/28.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/29.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/30.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/31.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/32.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/33.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/34.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/35.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/36.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/37.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/38.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/39.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/40.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/41.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/42.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/43.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/44.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/45.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/46.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/47.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/48.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/49.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/50.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/51.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/52.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/53.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/54.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/55.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/56.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/57.jpg",
+ },
+ {
+  imageUrl: "https://ashtavakra.vercel.app/58.jpg",
+ },
+ {
+  imageUrl: "https://ashtavakra.vercel.app/59.jpg",
+ },
+ {
+  imageUrl: "https://ashtavakra.vercel.app/60.jpg",
+ },
+ {
+  imageUrl: "https://ashtavakra.vercel.app/61.jpg",
+ },
+ {
+  imageUrl: "https://ashtavakra.vercel.app/62.jpg",
+ },
+ {
+  imageUrl: "https://ashtavakra.vercel.app/63.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/64.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/65.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/66.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/67.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/68.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/69.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/70.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/71.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/72.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/73.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/74.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/75.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/76.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/77.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/78.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/79.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/80.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/81.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/82.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/83.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/84.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/85.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/86.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/87.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/88.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/89.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/90.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/91.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/92.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/93.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/94.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/95.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/96.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/97.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/98.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/99.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/100.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/101.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/102.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/103.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/104.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/105.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/106.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/107.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/108.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/109.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/110.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/111.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/112.jpg",
+ },
+ {
+   imageUrl: "https://ashtavakra.vercel.app/113.jpg",
+ },{
+   imageUrl: "https://ashtavakra.vercel.app/114.jpg",
+ },
+{
+  imageUrl: "https://ashtavakra.vercel.app/115.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/116.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/117.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/118.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/119.jpg",
+},{
+  imageUrl: "https://ashtavakra.vercel.app/120.jpg",
+},
+
+{
+  imageUrl: "https://ashtavakra.vercel.app/121.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/122.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/123.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/124.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/125.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/126.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/127.jpg",
+},{
+  imageUrl: "https://ashtavakra.vercel.app/128.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/129.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/130.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/131.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/132.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/133.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/134.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/135.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/136.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/137.jpg",
+},{
+  imageUrl: "https://ashtavakra.vercel.app/138.jpg",
+},
+
+{
+  imageUrl: "https://ashtavakra.vercel.app/139.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/140.jpg",
+},
+{
+  imageUrl: "https://ashtavakra.vercel.app/141.jpg",
+},
+];
+
+export default pages;
